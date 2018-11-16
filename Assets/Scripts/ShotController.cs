9fbfce2d127a95214695617c488176d31379de6c
yuya <yuya_tanaka@di-v.co.jp>
2018-11-17 01:45:19 +0900

    条件分岐の分散

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -32,7 +32,7 @@ public class ShotController : MonoBehaviour
         shotInterval = 0;
         reloadInterval = 1;
         gunAudioSource = gameObject.GetComponent<AudioSource>();
-
+		
     }
 
     // Update is called once per frame
@@ -67,26 +67,40 @@ public class ShotController : MonoBehaviour
 
 
 
-        if (shotInterval >= 0.5f && reloadInterval >= 2f && _bullet > 0)
+        if (_bullet > 0)
         {
-           
 
-            _bullet -= 1;
 
-            gunAudioSource.PlayOneShot(shotSound);
-            shotRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
-            if (Physics.Raycast(shotRay, out gunShotHit))
+            if (shotInterval >= 0.5f)
             {
-                hitObjPosition = gunShotHit.point;
 
-                Instantiate(shotEffect, this.transform.position, Quaternion.identity);
-                Instantiate(shotReachEffect, hitObjPosition, Quaternion.identity);
+
+
+                if (reloadInterval >= 2f)
+                {
+
+                    _bullet -= 1;
+					shotInterval = 0;
+					print(_bullet);
+
+                    gunAudioSource.PlayOneShot(shotSound);
+                    shotRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+                    if (Physics.Raycast(shotRay, out gunShotHit))
+                    {
+                        hitObjPosition = gunShotHit.point;
+
+                        Instantiate(shotEffect, this.transform.position, Quaternion.identity);
+                        Instantiate(shotReachEffect, hitObjPosition, Quaternion.identity);
+
+
+                 
+
+                    }
+                }
 
             }
 
-            shotInterval = 0;
-            print(_bullet);
-
+            return;
 
         }
 
@@ -95,27 +109,53 @@ public class ShotController : MonoBehaviour
     }
     private void ReloadBullet()
     {
-        if (_bullet < 30 && _bulletBox > 0)
+        if (_bullet < 30)
         {
-          
-
-            gunAudioSource.PlayOneShot(reroadSound);
-            for (int i = 1; _bullet < 30; ++i)
+            if (_bulletBox > 0)
             {
-                if (_bulletBox > 0)
+                if (shotInterval >= 0.5f)
                 {
-                    _bullet += 1;
-                    _bulletBox -= 1;
-                    print(_bullet);
-                    print(_bulletBox);
+                    if (reloadInterval >= 2f)
+                    {
+
+
+
+                        gunAudioSource.PlayOneShot(reroadSound);
+                        for (int i = 1; _bullet < 30; ++i)
+                        {
+                            if (_bulletBox > 0)
+                            {
+                                _bullet += 1;
+                                _bulletBox -= 1;
+                                print(_bullet);
+                                print(_bulletBox);
+
+
+                            }
+
+
+
+
+
+                        }
+
+                    }
+
+
+
 
                 }
+                reloadInterval = 0;
 
             }
-            reloadInterval = 0;
+
+
+
 
 
         }
+
+        return;
 
 
     }
