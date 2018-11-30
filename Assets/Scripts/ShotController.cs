0119da85ed46ceaa1cbe2bf0891ed4d229a482e8
yuya <yuya_tanaka@di-v.co.jp>
2018-12-01 00:23:30 +0900

    ・ターゲットの配置 ・ライフの設定 ・アニメーションの設定 ・得点の設定 ・回復時間の設定

--- Assets/Scripts/ShotController.cs
+++ Assets/Scripts/ShotController.cs
@@ -17,6 +17,8 @@ public class ShotController : MonoBehaviour
     private Vector3 hitObjPosition;
     private float shotInterval;
     private float reloadInterval;
+
+    private float hitObjPosY;
     private AudioSource gunAudioSource;
 
     private const int RELOAD_BORDER_TIME = 2;
@@ -25,11 +27,9 @@ public class ShotController : MonoBehaviour
 
     private const int BULLET_STOCK_FULL = 30;
 
+    private const int HIT_HEADMARKER_SCORE = 50;
 
-
-
-
-
+    private const int HIT_TARGET_SCORE = 3;
 
     // Use this for initialization
     void Start()
@@ -37,7 +37,7 @@ public class ShotController : MonoBehaviour
         shotEffect = Resources.Load<GameObject>("Effects/ShotEffect");
         shotReachEffect = Resources.Load<GameObject>("Effects/ShotReachEffect");
         gunAudioSource = gameObject.GetComponent<AudioSource>();
-
+        print(TargetController._targetHP);
     }
 
     // Update is called once per frame
@@ -55,7 +55,6 @@ public class ShotController : MonoBehaviour
         {
             ReloadBullet();
         }
-
     }
     private void ShotGun()
     {
@@ -76,24 +75,24 @@ public class ShotController : MonoBehaviour
         }
         _bullet -= 1;
         shotInterval = 0;
-        print(_bullet);
+        // print(_bullet);
 
         gunAudioSource.PlayOneShot(shotSound);
         shotRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
         if (Physics.Raycast(shotRay, out gunShotHit))
         {
-            hitObjPosition = gunShotHit.point;
+            hitObjPosition = gunShotHit.point; 
+
+            hitObjPosY = hitObjPosition.y;
 
             Instantiate(shotEffect, this.transform.position, Quaternion.identity);
             Instantiate(shotReachEffect, hitObjPosition, Quaternion.identity);
+
+            HitTargetScoreHp();
         }
-
     }
-
-
     private void ReloadBullet()
     {
-
         if (_bulletBox == 0)
         {
             return;
@@ -106,7 +105,6 @@ public class ShotController : MonoBehaviour
         {
             return;
         }
-
         if (_bullet >= BULLET_STOCK_FULL)
         {
             return;
@@ -119,14 +117,28 @@ public class ShotController : MonoBehaviour
             {
                 _bullet += 1;
                 _bulletBox -= 1;
-                print(_bullet);
-                print(_bulletBox);
-
+                // print(_bullet);
+                // print(_bulletBox);
             }
         }
-
     }
 
+    private void HitTargetScoreHp()
+    {
 
-
+        if (gunShotHit.collider.gameObject.tag == "HeadMarker")
+        {
+            TargetController._targetHP--;
+            TargetController._targetScore += HIT_HEADMARKER_SCORE;
+            print(TargetController._targetHP);
+            print(TargetController._targetScore);
+        }
+        if (gunShotHit.collider.gameObject.tag == "Target")
+        {
+            TargetController._targetHP--;
+            TargetController._targetScore += hitObjPosY * HIT_TARGET_SCORE;
+            print(TargetController._targetHP);
+            print(TargetController._targetScore);
+        }
+    }
 }
---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TargetController : MonoBehaviour
+{
+    public static int _targetHP = 5;
+    public static float _targetScore;
+    private Animator targetMotion;
+    private const int TARGET_HP_EMPTY = 0;
+    private const int TARGET_HP_FULL = 5;
+    private const int REVIVE_MOTION_INTERVAL = 2;
+    private const int DEFAULT_MOTION_INTERVAL = 1;
+    private bool _isCrushTarget;
+    private bool _isReviveTarget;
+    private bool _isDefaultTarget;
+
+    // Use this for initialization
+    void Start()
+    {
+        targetMotion = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_targetHP == TARGET_HP_EMPTY)
+        {
+            TargetCrashMotion(ref _isCrushTarget, ref _isReviveTarget);
+            StartCoroutine(TargetReviveMotion(_isReviveTarget));
+            print(_targetHP);
+        }
+        else
+        {
+            StopTargetMotion();
+        }
+    }
+    private void TargetCrashMotion(ref bool _isCrushTarget, ref bool _isReviveTarget)
+    {
+        _isCrushTarget = true;
+        _isReviveTarget = true;
+        targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+    }
+    private IEnumerator TargetReviveMotion(bool _isReviveTarget)
+    {
+        if (_isReviveTarget)
+        {
+            _targetHP = TARGET_HP_FULL;
+            // print("b");
+            yield return new WaitForSeconds(REVIVE_MOTION_INTERVAL);
+            targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+            yield return new WaitForSeconds(DEFAULT_MOTION_INTERVAL);
+            _isDefaultTarget = true;
+            targetMotion.SetBool("IsDefaultTarget", _isDefaultTarget);
+            // targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+            // targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+            yield break;
+        }
+    }
+    private void StopTargetMotion()
+    {
+        _isCrushTarget = false;
+        targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+        _isReviveTarget = false;
+        targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+        _isDefaultTarget = false;
+        targetMotion.SetBool("IsDefaultTarget", _isDefaultTarget);
+    }
+}
