31bb98426844a310431d4d8861f79d4ff5a08b94
yuya <yuya_tanaka@di-v.co.jp>
2018-11-30 23:44:56 +0900

    Revert "a"
    This reverts commit 346e762ab7930f70bec602e966466dae38988a5d.

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
 public class TargetController : MonoBehaviour
 {
     public static int _targetHP = 5;
@@ -14,25 +13,58 @@ public class TargetController : MonoBehaviour
     private bool _isCrushTarget;
     private bool _isReviveTarget;
     private bool _isDefaultTarget;
-=======
-public class TargetController : MonoBehaviour {
->>>>>>> parent of 59554be... アニメーションの設定、回復時間の設定
 
+    // Use this for initialization
+    void Start()
+    {
+        targetMotion = GetComponent<Animator>();
+    }
 
-	public int _targetHP;
-	public float _targetScore;
+    // Update is called once per frame
+    void Update()
+    {
+        if (_targetHP == TARGET_HP_EMPTY)
+        {
+            TargetCrashMotion(ref _isCrushTarget, ref _isReviveTarget);
+            StartCoroutine(TargetReviveMotion(_isReviveTarget));
+            print(_targetHP);
+        }
+        else
+        {
+            StopTargetMotion();
+        }
+    }
 
-	
+    private void TargetCrashMotion(ref bool _isCrushTarget, ref bool _isReviveTarget)
+    {
+        _isCrushTarget = true;
+        _isReviveTarget = true;
+        targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+    }
+    private IEnumerator TargetReviveMotion(bool _isReviveTarget)
+    {
+        if (_isReviveTarget)
+        {
+            _targetHP = TARGET_HP_FULL;
+            // print("b");
+            yield return new WaitForSeconds(REVIVE_MOTION_INTERVAL);
+            targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+            yield return new WaitForSeconds(DEFAULT_MOTION_INTERVAL);
+            _isDefaultTarget = true;
+            targetMotion.SetBool("IsDefaultTarget", _isDefaultTarget);
+            // targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+            // targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+            yield break;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	
+        }
+    }
+    private void StopTargetMotion()
+    {
+        _isCrushTarget = false;
+        targetMotion.SetBool("IsCrushTarget", _isCrushTarget);
+        _isReviveTarget = false;
+        targetMotion.SetBool("IsReviveTarget", _isReviveTarget);
+        _isDefaultTarget = false;
+        targetMotion.SetBool("IsDefaultTarget", _isDefaultTarget);
+    }
 }
